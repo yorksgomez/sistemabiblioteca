1a179dc12fdc14b819281875fbe6e9b086f90d14
Yorks Gomez <yorksgomez@gmail.com>
2022-11-09 15:07:52 -0500

    Proyecto inicializado

---
+++ src/main/java/edu/eci/cvds/SistemaBiblioteca/SistemaBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.SistemaBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/SistemaBiblioteca/SistemaBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.SistemaBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
