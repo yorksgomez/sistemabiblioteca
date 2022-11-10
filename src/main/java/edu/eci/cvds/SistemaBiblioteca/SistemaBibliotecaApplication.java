0b244bc01a23bcc443105ec1013d450257df373f
Yorks Gomez <yorksgomez@gmail.com>
2022-11-10 09:08:30 -0500

    fast deploy test

--- src/main/java/edu/eci/cvds/SistemaBiblioteca/SistemaBibliotecaApplication.java
+++ src/main/java/edu/eci/cvds/SistemaBiblioteca/SistemaBibliotecaApplication.java
@@ -17,7 +17,7 @@ public class SistemaBibliotecaApplication {
 	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hello test 1: %s!", name);
 	}
 
 }
