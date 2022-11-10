a568738fc4eb3faec9cbdebb3bb4a6c3106c5819
Yorks Gomez <yorksgomez@gmail.com>
2022-11-09 21:25:18 -0500

    Arreglado

--- src/main/java/edu/eci/cvds/SistemaBiblioteca/SistemaBibliotecaApplication.java
+++ src/main/java/edu/eci/cvds/SistemaBiblioteca/SistemaBibliotecaApplication.java
@@ -2,12 +2,22 @@ package edu.eci.cvds.SistemaBiblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class SistemaBibliotecaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaBibliotecaApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
