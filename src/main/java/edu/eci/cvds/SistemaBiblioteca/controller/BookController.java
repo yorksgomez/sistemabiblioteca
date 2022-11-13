e88d65c52da4549d5eb6f858f766b781f12f1880
Yorks Gomez <yorksgomez@gmail.com>
2022-11-12 19:54:09 -0500

    Cambios en base de datos y en template

--- src/main/java/edu/eci/cvds/SistemaBiblioteca/controller/BookController.java
+++ src/main/java/edu/eci/cvds/SistemaBiblioteca/controller/BookController.java
@@ -1,12 +1,13 @@
 package edu.eci.cvds.SistemaBiblioteca.controller;
 
+import javax.sql.DataSource;
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import edu.eci.cvds.SistemaBiblioteca.model.Book;
 import edu.eci.cvds.SistemaBiblioteca.services.impl.BookServiceImplementation;
 
@@ -15,6 +16,9 @@ public class BookController {
 
     @Resource(name = "bookService")
     private BookServiceImplementation service;
+
+    @Autowired
+    private DataSource dataSource;
 
     @GetMapping("/books")
     public String view(Model model) {
@@ -26,7 +30,7 @@ public class BookController {
             service.addBook(new Book(1l, "TITULO LIBRO DE PRUEBO", "AUTOR DE PRUEBA"));
         }catch(Exception ex) {System.out.println("Mensaje: " + ex.getMessage());}
         
-    
+        System.out.println("DATASOURCE: " + DataSourceUtils.getConnection(dataSource).toString());
         
         Book book = service.find(1l);
         model.addAttribute("book", book);
