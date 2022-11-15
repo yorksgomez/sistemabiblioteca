0e446697f14db633e2ead92e38ac87d676f6f3b2
Yorks Gomez <yorksgomez@gmail.com>
2022-11-14 21:07:49 -0500

    Arreglado modelo

--- src/main/java/edu/eci/cvds/SistemaBiblioteca/model/User.java
+++ src/main/java/edu/eci/cvds/SistemaBiblioteca/model/User.java
@@ -1,23 +1,42 @@
 package edu.eci.cvds.SistemaBiblioteca.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class  User  {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
     private String mail;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
+    private String role;
 
     public User() {
-
+        this.mail = "";
+        this.name = "";
+        this.password = "";
+        this.role = "";
     }
 
-    public User( String mail, String name, String password) {
-
+    public User( String mail, String name, String password, String role) {
         this.mail = mail;
         this.name = name;
         this.password = password;
-
+        this.role = role;
     }
 
 
