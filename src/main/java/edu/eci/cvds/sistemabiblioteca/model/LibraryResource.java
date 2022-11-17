8d77347c9c70135d6611f3038a64e68a4bd7acfb
Yorks Gomez <yorksgomez@gmail.com>
2022-11-17 15:26:27 -0500

    Agreado modelo

--- src/main/java/edu/eci/cvds/sistemabiblioteca/model/LibraryResource.java
+++ src/main/java/edu/eci/cvds/sistemabiblioteca/model/LibraryResource.java
@@ -1,11 +1,31 @@
 package edu.eci.cvds.sistemabiblioteca.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class LibraryResource {
     // To be implemented
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column
+    private String nombre;
+
+    @Column
+    private String ubicacion;
+
+    @Column
+    private String tipo;
+
+    @Column
+    private Integer capacidad;
+
+    @Column
+    private String disponibilidad;
+
 }
--- src/main/java/edu/eci/cvds/sistemabiblioteca/services/exc/LibraryHistoryException.java
+++ src/main/java/edu/eci/cvds/sistemabiblioteca/services/exc/LibraryHistoryException.java
@@ -1,4 +1,4 @@
-package edu.eci.cvds.sistemabiblioteca.services;
+package edu.eci.cvds.sistemabiblioteca.services.exc;
 
 public class LibraryHistoryException extends  Exception{
     private static final long serialVersionUID = 5758585917279118959L;
