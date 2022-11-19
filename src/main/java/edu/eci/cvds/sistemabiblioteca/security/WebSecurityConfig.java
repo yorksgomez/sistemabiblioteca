0f41d224f601eea2d5951c1f5707acd8c2508da9
Jorge <saenzdiazj0@gmail.com>
2022-11-18 22:50:33 -0500

    Add email pattern

--- src/main/java/edu/eci/cvds/sistemabiblioteca/security/WebSecurityConfig.java
+++ src/main/java/edu/eci/cvds/sistemabiblioteca/security/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/books").authenticated()
+            .antMatchers("/").authenticated()
             .anyRequest().permitAll()
             .and()
             .formLogin()
