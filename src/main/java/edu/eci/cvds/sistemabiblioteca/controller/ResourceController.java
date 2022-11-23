a21ae6d5a1a808d91a0ae81d119a388dc203c759
Luciernagas <104604359+Luciernagas@users.noreply.github.com>
2022-11-23 17:15:43 -0500

    ResourceController implementation

--- src/main/java/edu/eci/cvds/sistemabiblioteca/controller/ResourceController.java
+++ src/main/java/edu/eci/cvds/sistemabiblioteca/controller/ResourceController.java
@@ -6,8 +6,11 @@ import edu.eci.cvds.sistemabiblioteca.services.impl.ResourceServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 public class ResourceController {
@@ -20,4 +23,27 @@ public class ResourceController {
         model.addAttribute("resource", new LibraryResource());
         return "resource";
     }
+    
+    @PostMapping("/getResource")
+    public List<LibraryResource> getResource(LibraryResource libraryResource){
+        HashMap<String, Object> data = new HashMap<>();
+
+        String resourceName = libraryResource.getName();
+        String resourceLocation = libraryResource.getLocation();
+        String resourceType = libraryResource.getType();
+        int resourceCapacity = libraryResource.getCapacity();
+
+        if (!resourceName.equals(""))
+            data.put("name", resourceName);
+        if (!resourceLocation.equals(""))
+            data.put("location", resourceLocation);
+        if (!resourceType.equals(""))
+            data.put("type", resourceType);
+        if (resourceCapacity != -1) {
+            data.put("capacity", resourceCapacity);
+        }
+
+        return resourceService.findResource(data);
+    }
+
 }
