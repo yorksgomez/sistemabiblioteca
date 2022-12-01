94354a2d719c88410cb1dc01604fead50d035fce
Yorks Gomez <yorksgomez@gmail.com>
2022-11-30 23:50:54 -0500

    Jorge luisa daño esto

---
+++ src/main/resources/static/scripts/resources.js
@@ -0,0 +1,36 @@
+function doSearch() {
+    var tableReg = document.getElementById('regTable');
+    var searchText = document.getElementById('searchTerm').value.toLowerCase();
+    for (var i = 1; i < tableReg.rows.length; i++) {
+        var cellsOfRow = tableReg.rows[i].getElementsByTagName('td');
+        var found = false;
+        for (var j = 0; j < cellsOfRow.length && !found; j++) {
+            var compareWith = cellsOfRow[j].innerHTML.toLowerCase();
+            if (searchText.length == 0 || (compareWith.indexOf(searchText) > -1)) {
+                found = true;
+            }
+        }
+        if (found) {
+            tableReg.rows[i].style.display = '';
+        } else {
+            tableReg.rows[i].style.display = 'none';
+        }
+    }
+}
+
+function sortTable(table, col, reverse) {
+    var tb = table.tBodies[0], // use `<tbody>` to ignore `<thead>` and `<tfoot>` rows
+        tr = Array.prototype.slice.call(tb.rows, 0), // put rows into array
+        i;
+    reverse = -((+reverse) || -1);
+    tr = tr.sort(function (a, b) { // sort rows
+        return reverse // `-1 *` if want opposite order
+            * (a.cells[col].textContent.trim() // using `.textContent.trim()` for test
+                    .localeCompare(b.cells[col].textContent.trim(), undefined, {numeric: true})
+            );
+    });
+    for(i = 0; i < tr.length; ++i) {
+        tb.appendChild(tr[i]); // append each row in order
+
+    }
+}
