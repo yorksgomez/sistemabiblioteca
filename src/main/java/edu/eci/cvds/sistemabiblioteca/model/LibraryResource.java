d26ee3a6f68b9984365ea11bdb24cb23dbfd7830
Jorge <saenzdiazj0@gmail.com>
2022-11-30 22:34:37 -0500

    Patch bug

--- src/main/java/edu/eci/cvds/sistemabiblioteca/model/LibraryResource.java
+++ src/main/java/edu/eci/cvds/sistemabiblioteca/model/LibraryResource.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Calendar;
+import java.util.Date;
 
 @Entity
 public class LibraryResource {
@@ -13,7 +15,7 @@ public class LibraryResource {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column
+    @Column(unique = true)
     private String name;
 
     @Column
