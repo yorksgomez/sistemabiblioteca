2e68a105407fac324b7cf9057abd9be7d40652f5
CamiloCastiblanco <69698380+CamiloCastiblanco@users.noreply.github.com>
2022-12-01 22:56:43 -0500

    registerResourceServiceFixed

--- src/main/java/edu/eci/cvds/SistemaBiblioteca/model/LibraryResource.java
+++ src/main/java/edu/eci/cvds/SistemaBiblioteca/model/LibraryResource.java
@@ -2,13 +2,46 @@ package edu.eci.cvds.sistemabiblioteca.model;
 
 import edu.eci.cvds.sistemabiblioteca.model.interfaces.Models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
 
+class MyKey implements Serializable{
+    private Long id;
+
+
+    private String name;
+
+
+    private String location;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+
+}
 @Entity
+@IdClass(MyKey.class)
 public class LibraryResource implements Models {
 
     @Id
@@ -18,7 +51,7 @@ public class LibraryResource implements Models {
     @Column(unique = true)
     private String name;
 
-    @Column
+    @Column(unique = true)
     private String location;
 
     @Column
